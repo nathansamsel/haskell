d4cf0c966a29d88efb7490922ac4d91124b150e1
Nathan Egan <egan.nathan@gmail.com>
2016-10-17 15:32:20 -0600

    added factorial functions

--- factorial.hs
+++ factorial.hs
@@ -1,1 +1,10 @@
 factOne n = if n==0 then 1 else n * factOne(n-1)
+
+factTwo 0 = 1
+factTwo n = n * factTwo(n-1)
+
+factThree x
+  | x > 1 = x * factThree(x-1)
+  | otherwise = 1
+
+factFour n = product [1..n]
