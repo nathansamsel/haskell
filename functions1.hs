805a8bc4733519386875e898a1bb1e6ee7c5b70b
Nathan Egan <egan.nathan@gmail.com>
2016-10-31 21:10:08 -0600

    added 10 functions

--- functions1.hs
+++ functions1.hs
@@ -1,19 +1,43 @@
-rootList ::
+--applies sqrt to each element of a list
+rootList :: [Double] -> [Double]
+rootList xs = map sqrt xs
 
-multiplyPairs ::
+--multiplies the elements of tuples and returns them in list
+--tuple product corresponds to index of original tuple
+multiplyPairs :: [(Int, Int)] -> [Int]
+multiplyPairs = map (\(x,y) -> x*y)
 
-incrementList ::
+--increments the input list by the input increment
+incrementList :: Int -> [Int] -> [Int]
+incrementList z x = map (\x -> x+z) x
 
-squareSum ::
+--sums the squares of each element from the input list
+squareSum :: [Int] -> Int
+squareSum (x:xs) = foldl (\acc x -> acc + (x * x)) x xs 
 
-booleanOr ::
+--executes a logical OR on the whole list
+booleanOr :: [Bool] -> Bool
+booleanOr [] = False 
+booleanOr (x:xs) = foldl (\acc x -> acc || x) x xs
 
-booleanAnd ::
+--executes a logical AND on the whole list
+booleanAnd :: [Bool] -> Bool
+booleanAnd [] = True
+booleanAnd (x:xs) = foldl (\acc x -> acc && x) x xs
 
-dupList ::
+--duplicates each element of input list
+dupList :: [a] -> [a]
+dupList = (>>= replicate 2)
 
-trueCount ::
+--counts occurrences of True in a list
+--off by one
+trueCount :: [Bool] -> Int
+trueCount x = foldl (\acc x -> if x then acc+1 else acc) 0 x 
 
-rotateTriples ::
+--rearranges triple-tuple in specified way
+rotateTriples :: [(Int, Int, Int)] -> [(Int, Int, Int)]
+rotateTriples = map (\(x,y,z) -> (y, z, x))
 
-concatLists ::
+--flattens a list of lists of ints into a list of ints with same elements
+concatLists :: [[Int]] -> [Int]
+concatLists xss = foldl (++) [] xss 
